0cd8d771e9ff3211fea3949799bc099ace3b8f8e
Agustin Camejo <agustincamejo03@gmail.com>
2026-05-03 20:53:59 -0300

    fix build

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Services.scss';
 import { SectionTitle } from '../section-title/SectionTitle';
 import AOS from 'aos';
